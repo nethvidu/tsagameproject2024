32e0adacc6f8a96ff3aa4afd453b0ebfd8a2d0c2
nethvidu <saniru911@gmail.com>
2025-03-11 21:24:57 -0700

    MenuQuitFunction

--- TSAGameProject/Assets/Scriptsorsmth/UI/MainMenu.cs
+++ TSAGameProject/Assets/Scriptsorsmth/UI/MainMenu.cs
@@ -7,4 +7,11 @@ public class MainMenu : MonoBehaviour
     public void LoadScene(string scene){
         SceneManager.LoadScene(scene);
     }
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+        #else
+        Application.Quit(); 
+        #endif
+    }
 }
